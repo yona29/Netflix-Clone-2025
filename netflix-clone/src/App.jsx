954e1b8735c7185a7071d49dbbeb564840288940
yona29 <yoniyehabtishleg@gmail.com>
2025-09-02 19:24:48 -0400

    frist-commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+ 
+  return (
+    <div className='app'>
+      <h1>hello</h1>
+    </div>
+  );
+     
+}
+
+export default App
