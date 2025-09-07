2869d8382bf48435c948eb3d4a85e19da1200b3a
yona29 <yoniyehabtishleg@gmail.com>
2025-09-07 13:17:07 -0400

    deploiment part update

--- netflix-clone/src/assets/commponents/Rows/Row/Row.jsx
+++ netflix-clone/src/assets/commponents/Rows/Row/Row.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState}from 'react';
 import "./row.css";
 import axios from '../../../../utils/axios';
 import movieTrailer from 'movie-trailer';
-import YouTube from 'react-YouTube'
+import YouTube from "react-youtube";
 
 const Row = ({title,fetchUrl,isLargeRow}) => {
   const[movies,setMovie] = useState([]);
